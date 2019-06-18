354e12b3b0967481a8089077d2905c3b5bec25f3
Rob Paik <robpaik@gmail.com>
2019-06-18 01:35:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.random() * Math.floor()
+ var price = Math.floor(Math.random() * Math.floor(max));
 }
 
 function viewCart() {
